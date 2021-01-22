3b49fcbe8bf796b5e827ae5bfc3f3c5a4fb49f12
Leo Huang <77544794+leogjhuang@users.noreply.github.com>
2021-01-21 23:41:01 -0700

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    ;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int tc = 1;
+    for (int i = 1; i <= tc; ++i) {
+        solve();
+    }
+}
